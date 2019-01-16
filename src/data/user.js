1c41017c2b6062e85ad86cd0dc5285517d11bb8b
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-16 14:37:06 -0200

    Define

--- src/data/user.js
+++ src/data/user.js
@@ -1,7 +1,9 @@
 module.exports = {
   user: {
-    name: 'ViniTemplateEngine',
-    description: 'Micro Static Site Generator in Node.js',
-    github_url: 'https://github.com/ViniciusGularte'
+    name: 'Vinicius Gularte Martin',
+    description: 'Developer from Brazil',
+    github_url: 'https://github.com/ViniciusGularte',
+    linkedin_url:'https://www.linkedin.com/in/viniciusgularte/',
+    github_image:'https://avatars3.githubusercontent.com/u/25805846?s=460&v=4'
   }
 };

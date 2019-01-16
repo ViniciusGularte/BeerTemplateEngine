8c08efcad3673acf5206568513a006f7c6c5f1d8
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-16 13:39:23 -0200

    refactoring

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ module.exports = {
   site: {
     title: 'ViniTemplateEngine',
     description: 'Micro Static Site Generator in Node.js',
-    basePath: process.env.NODE_ENV === 'production' ? '/github' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/' : '',
   },
   build: {
     outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'

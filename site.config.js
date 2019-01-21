5d55d871da519defebfdae16fc6bd783f4b84c24
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-21 11:06:22 -0200

    fix env variables

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'Micro Static Site Generator in Node.js',
     author:'Vinicius Gularte',
     footer:'BeerTemplate',
-    basePath: process.env.NODE_ENV === 'production' ? '/' : 'https://viniciusgularte.github.io/BeerTemplate',
+    basePath: process.env.NODE_ENV === 'production' ? '/' : 'https://viniciusgularte.github.io/BeerTemplateEngine',
   },
   build: {
     outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'

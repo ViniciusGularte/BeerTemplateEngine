227c69bc3660757dfb2834625d364a74b4a77b58
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-21 18:03:42 -0200

    refactoring env variables

--- site.config.js
+++ site.config.js
@@ -4,9 +4,6 @@ module.exports = {
     description: 'Micro Static Site Generator in Node.js',
     author:'Vinicius Gularte',
     footer:'BeerTemplate',
-    basePath: process.env.NODE_ENV === 'production' ? '/' : 'https://viniciusgularte.github.io/BeerTemplateEngine',
-  },
-  build: {
-    outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'
+    basePath: process.env.NODE_ENV === 'production' ? '' : 'https://viniciusgularte.github.io/BeerTemplateEngine',
   }
 };

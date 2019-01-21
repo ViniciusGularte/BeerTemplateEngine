9168951e800fd40a488bce052ed7b4415e6fc3dd
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-21 19:06:01 -0200

    add more customize content

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 module.exports = {
   user: {
-    site_title:'Vinicius Gularte',
+    site_title:'Vinicius Gularte Web Developer',
     name: 'Vinicius Gularte Martin',
-    description: 'Developer from Brazil',
+    description: 'Developer from Brazil and writer in medium',
     github_url:  'https://github.com/ViniciusGularte',
     linkedin_url:'https://www.linkedin.com/in/viniciusgularte/',
     github_image:'https://avatars3.githubusercontent.com/u/25805846?s=460&v=4',
